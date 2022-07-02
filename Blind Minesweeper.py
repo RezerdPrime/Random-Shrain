654f813cedcad96b2b98f5843045069a81bd671e
RezerdPrime <101434848+RezerdPrime@users.noreply.github.com>
2022-07-02 23:07:09 +1000

    Add files via upload
    Something looking similar at Minesweeper but you can't see the field :P

---
+++ Blind Minesweeper.py
@@ -0,0 +1,45 @@
+Lenght_of_field = int(input('Set lenght of your field: '))
+Height_of_field = int(input('Set height of your field: '))
+
+from random import randint
+
+sField = ''
+for iL in range(Lenght_of_field):
+    for iH in range(Height_of_field):
+        sField += str(randint(0,1))
+    sField += '\n'                  # Field generation #
+#-----------------------------------------------------------------------------#
+print('')
+dikt = set()
+k1 = 0; k0 = int((sField.count('1')/2 + sField.count('0')/2)/2) + 2
+while k1 != sField.count('1') or k0 != 0:
+    
+    print('Fail maximum:',k0)
+    print('Your score:',k1)
+    print('Your moves:',sorted(dikt))
+    x=int(input('Set value of X pos (1-5): '))-1
+    y=int(input('Set value of Y pos (1-5): '))-1
+    print('\n')
+    
+    file=open('game.txt','w')
+    file.write(sField)
+    file.close()
+    strFile=open('game.txt').readlines()
+    # print(srtFile[y],strFile[y][x])
+    
+    import os
+    os.remove('game.txt')           # Setting 2D position and field reading #
+#-----------------------------------------------------------------------------#
+    gMove=str(x+1) + str(y+1)
+    if strFile[y][x] == '1' and gMove not in dikt:
+        k1 += 1
+        dikt.add(gMove)
+    else: k0 -=1 ; dikt.add(gMove)  # Condition of score and fail maximum #
+#-----------------------------------------------------------------------------#   
+    if k1 == sField.count('1'):
+        print('Victory!\n' + sField)
+        break
+    if k0 == 0:
+        print('Lose\n' + sField)
+        break                       # Win/Lose condition #
+#-----------------------------------------------------------------------------#
