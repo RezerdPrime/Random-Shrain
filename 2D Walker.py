b9fef40dcea0b7f4d79bf37d737dbca152d196c9
RezerdPrime <101434848+RezerdPrime@users.noreply.github.com>
2022-07-19 17:26:20 +1000

    Update 2D Walker.py

--- 2D Walker.py
+++ 2D Walker.py
@@ -5,13 +5,15 @@ Yfield = int(input('Set height of field: '))
 from random import randint
 import os
 
-Repeats = 1
+Repeats = 1; comment = ''; Player_score = 0
 while Repeats == 1:
     field = '*' * (Xfield + 2) + '\n'
     field_elements = [' ',' ',' ',' ',' ',' ',' ',' ','X','X','*']
-    if (Xfield < 3) or (Yfield < 3):
-        field_elements = [' ','X','*']
-
+    
+    if (Xfield <= 3 and Yfield >= 10) or (Xfield <= 3 and Yfield <= 3):
+        field_elements = [' ','X','X','*','*']
+        comment = "But it's unplayable field lmao"
+        
     for iY in range(Yfield):
         field += '*'
         for iX in range(Xfield):
@@ -32,19 +34,20 @@ while Repeats == 1:
         else: print(maxStrcount)
     print('\n')
 
-    if maxStrcount > 1:
-        Repeats = 1
+    if (maxStrcount > 1) or (field.count(' ') == 0): Repeats = 1
     else: Repeats = 0
-print('Generation successful!\n')                            # Re-generation system
+print('Generation successful!',comment,'\n')                 # Re-generation system
 #---------------------------------------------------------------------------------#
 place_list = []
 for iY2 in range(len(gamedata)):
     for iX2 in range(len(gamedata[iY2])):
         if gamedata[iY2][iX2] == ' ':
             place_list.append(str(iX2) + '-' + str(iY2))      # List of free pos'es
-#---------------------------------------------------------------------------------#           
+#---------------------------------------------------------------------------------#
 Xpos = 0; Ypos = 0
 Userpos = place_list[randint(0,len(place_list)-1)]
+place_list.remove(Userpos)
+
 for i3 in range(len(Userpos)):
     if Userpos[i3] == '-':
         Xpos = int(Userpos[:i3])
@@ -60,15 +63,49 @@ for i5 in range(Xpos + 1,len(Userstr)):
     str2 += Userstr[i5]
 
 field = field.replace(Userstr,str1 + 'O' + str2)
-print(field)
 gamedataNew = open('gamedataNew.txt','w')
 gamedataNew.write(field)
 gamedataNew.close()                                          # Adding of the player
 #---------------------------------------------------------------------------------#
+Condition_of_win = int(((field.count('X') + field.count(' ') + field.count('*')) / ((Xfield + Yfield) / 2)) / 2)
+counter = 0
+while counter < Condition_of_win:
+
+    Xpos_xp = 0; Ypos_xp = 0
+    XP_pos = place_list[randint(0,len(place_list)-1)]
+    place_list.remove(XP_pos)
+    
+    for i3 in range(len(XP_pos)):
+        if XP_pos[i3] == '-':
+            Xpos_xp = int(XP_pos[:i3])
+            Ypos_xp = int(XP_pos[i3 + 1:])                          # Random XP pos
+#---------------------------------------------------------------------------------#
+    gamedataNew = open('gamedataNew.txt').readlines()
+    XP_str = gamedataNew[Ypos_xp]
+    str1 = ''; str2 = ''
+
+    for i4 in range(Xpos_xp):
+        str1 += XP_str[i4]
+    for i5 in range(Xpos_xp + 1,len(XP_str)):
+        str2 += XP_str[i5]
+
+    field = field.replace(XP_str,str1 + '1' + str2)
+    gamedataNew = open('gamedataNew.txt','w')
+    gamedataNew.write(field)
+    gamedataNew.close()
+    counter += 1
+
+print(field)
+field_without_player = field.replace('O',' ')
+gamedata = open('gamedata.txt','w')
+gamedata.write(field_without_player)
+gamedata.close()
+gamedata = open('gamedata.txt').readlines()                             # XP adding
+#---------------------------------------------------------------------------------#
 HP = 1
 while HP == 1:
     Move = int(input('Your move (1-8): '))                 # Condition of game life
-#---------------------------------------------------------------------------------#        
+#---------------------------------------------------------------------------------#  
     if (Move == 1) and (not('XO' in field)):
         gamedataNew = open('gamedataNew.txt').readlines()
         Xpos -= 1
@@ -235,6 +272,14 @@ while HP == 1:
         gamedataNew.close()
     elif Move == 8: print(Info+'\n'+field)             # (8) "Down+Right" movenment
 #---------------------------------------------------------------------------------#
+    if open('gamedata.txt').readlines()[Ypos][Xpos] == '1' and open('gamedataNew.txt').readlines()[Ypos][Xpos] == 'O':
+        Player_score += 1
+        field_new = field.replace('O',' ')
+        gamedata = open('gamedata.txt','w')
+        gamedata.write(field_new)
+        gamedata.close()
+        gamedata = open('gamedata.txt').readlines()               # XP-up condition
+#---------------------------------------------------------------------------------#
     if (Move > 8) or (Move < 1):
         print('\nHaha.. no')
         os.remove('gamedata.txt')
@@ -243,8 +288,13 @@ while HP == 1:
 
     if ((open('gamedata.txt').readlines()[Ypos][Xpos] == '*') and (open('gamedataNew.txt').readlines()[Ypos][Xpos] == 'O')) or (open('gamedataNew.txt').readlines()[0].count('O') > 0) or (open('gamedataNew.txt').readlines()[Yfield + 1].count('O') > 0):
         HP = 0
-        print('\nGame Over\nYour HP = 0')
+        print('\nGame Over','Your HP = 0','Your score = ' + str(Player_score), sep = '\n')
         os.remove('gamedata.txt')
-        os.remove('gamedataNew.txt')                      # Condition of game break
-#---------------------------------------------------------------------------------#
-                                                                   # By RezerdPrime
+        os.remove('gamedataNew.txt')
+
+    if Player_score == Condition_of_win:
+        print('\nVictory!\n' + 'Your score = ' + str(Player_score))
+        os.remove('gamedata.txt')
+        os.remove('gamedataNew.txt')
+        break                                             # Condition of game break
+#---------------------------------------------------------------------------------#     by RezerdPrime
