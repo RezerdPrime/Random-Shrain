fd1576ea2841253a52d63c9e9546baa5f704887a
RezerdPrime <101434848+RezerdPrime@users.noreply.github.com>
2023-04-21 23:52:30 +1000

    Update Power_Polynomial_Approximation.py

--- PythonShit/Power_Polynomial_Approximation.py
+++ PythonShit/Power_Polynomial_Approximation.py
@@ -21,3 +21,14 @@ while coords != "end":
     
 # https://matrixcalc.org/ru/slu.html
 # https://www.desmos.com/calculator/ipfsu2naxz?lang=ru
+
+'''
+The program builds a matrix based on a finite power series. 
+The number of entered points must be equal to the number of maximal degree in the polynomial.
+
+The resulting matrix is placed in a dedicated window on the matrix calculator website.
+The solution to the matrix is the necessary coefficients that specify the desired function itself.
+
+The decision column is copied and placed in the "l" list in Desmos. 
+The value of the variable "a" is the maximum degree of the polynomial and also the number of points for which the function is true.
+'''
