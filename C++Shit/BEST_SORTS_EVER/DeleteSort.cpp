cdea5c9f212d35e0259404b27bc67ee13aa9f00b
Oswald Steinhelß <101434848+RezerdPrime@users.noreply.github.com>
2023-05-23 20:11:47 +1000

    Create DeleteSort.cpp

---
+++ C++Shit/BEST_SORTS_EVER/DeleteSort.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int N; cin >> N;
+    int val, cur; cin >> val;
+    for (int i = 1; i < N; i++) { cin >> cur; }
+  
+    cout << "\n" << val << "\n";
+
+}
