70fc169d8d0fc57b5c38bc493a01b5e6ae28007b
Eden Lichmer <101434848+RezerdPrime@users.noreply.github.com>
2024-07-11 15:39:05 +1000

    Update app.py

--- PythonShit/flask_moment/app.py
+++ PythonShit/flask_moment/app.py
@@ -4,6 +4,9 @@ import numpy as np
 app = Flask(__name__)
 x = np.arange(0, 10, 0.1)
 a = 1
+time_arr = []
+
+connected_mk = 2
 
 @app.route('/')
 def index():
@@ -11,14 +14,15 @@ def index():
 
 
 # Генерация новых данных (10 точек) и преобразование в Python-friendly формат
-@app.route('/update', methods=['GET'])
+@app.route('/update', methods=['GET', 'POST'])
 def update_data():
     global x, a
     y = np.sin(x + a)
     a += 0.1
+    time_arr.append(a)
 
     # Преобразование чисел в формат Python-friendly для сериализации в JSON
-    data = {'x': x.tolist(), 'y': y.tolist()}
+    data = {'x': x.tolist(), 'y': y.tolist(), 'cmk': connected_mk, 'time': a}
     return jsonify(data)
 
 
