181b34ec223dfaee9dedc2b82f15ab336b5b3cb2
Eden Lichmer <101434848+RezerdPrime@users.noreply.github.com>
2024-08-21 17:16:16 +1000

    Create badapple_frame_build.py

---
+++ PythonShit/img_transform/badapple_frame_build.py
@@ -0,0 +1,84 @@
+import math
+import numpy as np
+import multiprocessing as mp
+from PIL import Image
+
+
+import time
+start = time.time()
+end = start
+S = start
+
+
+width, height = 1440, 1080
+X, Y = 0, 0
+s = 350
+reps = 1
+
+
+def mean_col(_p, eps, arr):
+    borderx = round(_p[0] - eps)
+    bordery = round(_p[1] - eps)
+    lst = []
+
+    for i in range(2 * eps):
+        for j in range(2 * eps):
+            if (-1 < borderx + i < width) and (-1 < bordery + j < height):
+                lst.append(arr[bordery + j, borderx + i])
+
+    l = len(lst)
+    Rsum = sum([lst[_][0] for _ in range(l)])
+    Gsum = sum([lst[_][1] for _ in range(l)])
+    Bsum = sum([lst[_][2] for _ in range(l)])
+
+    return round(Rsum / (l+.01)), round(Gsum / (l+.01)), round(Bsum / (l+.01))
+
+
+def frame(I):
+    global start, end
+    w = complex(-math.sin(math.tau * I / 1000)*.75, math.cos(math.tau * I / 1000)*.75)
+
+    img = Image.open(f"frames\\{I*2}.png")
+    pixar = np.array(img)
+    canvas = Image.new(img.mode, img.size)
+
+    for _j in range(height):
+        for _i in range(width):
+
+            z = complex((_i - width/2) / s + X, (height/2 - _j) / s + Y)
+
+            for _ in range(7):
+                try:
+                    z = z**2 + w
+                except OverflowError: ...
+
+            try:
+                if abs(z) < 2:
+                    col = mean_col(((z.real - X) * s + width/2, height/2 - (z.imag - Y) * s), reps, pixar)
+                    canvas.putpixel((_i, _j), col)
+            except OverflowError: ...
+
+    canvas.save(f"newframes\\{I*2}.png")
+
+    end = time.time()
+    print(I, end - start)
+    start = end
+
+
+def convert_parallel(start_frame, finish_frame):
+    num_processes = mp.cpu_count()
+    pool = mp.Pool(processes=num_processes)
+    pool.map(frame, range(start_frame, finish_frame + 1))
+
+    pool.close()
+    pool.join()
+
+
+
+if __name__ == '__main__':
+    mp.freeze_support()
+    convert_parallel(0, 6569)
+
+    #frame(10)
+
+    print(time.time() - S)
